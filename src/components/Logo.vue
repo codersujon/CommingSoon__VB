e752b6d32592e8ded14e18d212f47ad4171cac8a
unknown <csesujon155@gmail.com>
2024-10-02 12:51:28 -0700

    Unecessary Files Remove

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,7 +2,9 @@
 </script>
 <template>
     <div class="wrappic1 m-r-30 m-t-10 m-b-10">
-        <a href="https://codersujon.com/" target="_blank"><img src="/assets/images/icons/logo.png" alt="LOGO"></a>
+        <a href="https://codersujon.com/" target="_blank">
+            <h2 class="display-4 text-white font-weight-bold">CS</h2>
+        </a>
     </div>
 </template>
 
