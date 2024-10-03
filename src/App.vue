7f72f04d5c5767188463aa4abeef9583386644f8
unknown <csesujon155@gmail.com>
2024-10-03 12:29:00 -0700

    CountDown Set

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <script setup>
-import Logo from '@/components/Logo.vue'
-import SocialMediaIcon from './components/SocialMediaIcon.vue';
-import Newsletter from './components/Newsletter.vue';
-const ourws = "Our website is"
-const cs = "Coming Soon"
+  import Logo from '@/components/Logo.vue'
+  import SocialMediaIcon from './components/SocialMediaIcon.vue';
+  import Newsletter from './components/Newsletter.vue';
+  import CountDown from "@/components/CountDown.vue";
+  const ourws = "Our website is";
+  const cs = "Coming Soon";
 </script>
 
 <template>
   <div class="bg-img1 size1 overlay1 p-t-24">
     <div class="flex-w flex-sb-m p-l-80 p-r-74 p-b-175 respon5">
-      
       <!-- logo -->
       <Logo />
 
       <!-- Social Icon -->
       <SocialMediaIcon />
-
     </div>
 
     <div class="flex-w flex-sa p-r-200 respon1">
       <div class="p-t-34 p-b-60 respon3">
         <p class="l1-txt1 p-b-10 respon2">{{ ourws }}</p>
         <h3 class="l1-txt2 p-b-45 respon2 respon4">{{ cs }}</h3>
-        <div class="cd100"></div>
+        <CountDown />
       </div>
-
       <!-- Newsletter -->
       <Newsletter />
     </div>
---
+++ src/components/CountDown.vue
@@ -0,0 +1,78 @@
+<script setup>
+  import {ref, onMounted} from "vue";
+  // Project EndDate
+  const EndDate = new Date("Oct 15, 2024").getTime();
+
+  const d = ref("");
+  const h = ref("");
+  const m = ref("");
+  const s = ref("");
+
+  // Handle Project End Date
+  const handleProjectDate = () =>{
+      let now = new Date().getTime();
+      let gap = EndDate - now;
+
+      let second = 1000;
+      let minute = second * 60;
+      let hour = minute * 60;
+      let day = hour * 24;
+
+      d.value = Math.floor(gap / (day));
+      h.value = Math.floor(gap % (day) / (hour));
+      m.value = Math.floor(gap % (hour) / (minute));
+      s.value = Math.floor(gap % (minute) / (second));
+  }
+
+  // onMounted
+  onMounted(()=>{
+      setInterval(()=>{
+        handleProjectDate();
+      },1000); // Every 1000 milisecond
+  });
+
+
+</script>
+
+<template>
+  <div class="countdown d-flex">
+    <div class="time mx-2 text-center">
+      <h2 class="d">{{ d }}</h2>
+      <span>Days</span>
+    </div>
+    <div class="time mx-2 text-center">
+      <h2 class="h">{{ h }}</h2>
+      <span>Hours</span>
+    </div>
+    <div class="time mx-2 text-center">
+      <h2 class="m">{{m}}</h2>
+      <span>Minutes</span>
+    </div>
+    <div class="time mx-2 text-center">
+      <h2 class="s">{{s}}</h2>
+      <span>Seconds</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.time {
+  width: 120px;
+  background: #0d1629;
+  padding: 20px;
+  line-height: -10;
+  border-radius: 10px;
+  color: #fff;
+}
+
+.time h2{
+  font-size: 60px;
+  font-weight: bold;
+}
+
+.time span {
+  font-size: 20px;
+}
+
+</style>
